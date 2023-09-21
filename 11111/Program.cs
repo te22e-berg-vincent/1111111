50abf8d91c5aeb1da1eb20e99b3a81cf34a5aa08
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2023-09-21 10:55:20 +0200

    Commit rng+loop

---
+++ 11111/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*
+Random generator = new Random();
+
+int slumptal = generator.Next(10, 20);
+
+Console.WriteLine(slumptal);
+
+Console.ReadLine();
+//slump
+*/
+
+
+
+
+//while-loop
+int tal = 100;
+
+    while (tal > 0) {
+Console.WriteLine(tal);
+tal--;
+    }
+Console.WriteLine("fvgfdfgfdgfgfcdf");
+
+
+
+Console.ReadLine();
